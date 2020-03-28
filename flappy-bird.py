a0f73302d1dbb1fa14059763e2feb0df79879d57
DarkAlcid <sasuke_007@hotmail.fr>
2020-03-28 19:16:20 +0100

    Base class added

--- flappy-bird.py
+++ flappy-bird.py
@@ -117,6 +117,41 @@ class Pipe :
         top_mask = pygame.mask.from_surface(self.PIPE_TOP)
         bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
 
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+    
+        if t_point or b_point :
+            return True
+        
+        return False
+
+class Base :
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if (self.x1 + self.WIDTH < 0):
+            self.x1 = self.x2 + self.WIDTH
+        
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+        
 def draw_window(win, bird):
     win.blit(BG_IMG, (0,0))
     bird.draw(win)
