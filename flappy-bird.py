dde883351b685ab2ca77f24a25e975e5764d7f51
DarkAlcid <sasuke_007@hotmail.fr>
2020-03-28 19:10:24 +0100

    Pipe class added

--- flappy-bird.py
+++ flappy-bird.py
@@ -84,6 +84,39 @@ class Bird :
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
     
+class Pipe :
+    GAP = 200
+    VEL = 5 
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+    
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+    
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
 def draw_window(win, bird):
     win.blit(BG_IMG, (0,0))
     bird.draw(win)
@@ -94,7 +127,6 @@ def main():
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
 
-    draw_window(win, bird)
     run = True 
     while run :
         clock.tick(30)
